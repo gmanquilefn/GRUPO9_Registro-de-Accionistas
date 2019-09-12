f911e261e392654a64721e906dde25c56b2f6603
tostin <tostin737@gmail.com>
2019-09-11 21:03:12 -0300

    cambio de interfaz y busqueda

--- seguridad/views.py
+++ seguridad/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView
-from accionista.models import Accionista
-from django.db.models import Q
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
@@ -13,13 +11,4 @@ class HomePageView(LoginRequiredMixin, TemplateView):
 
 class BusquedaView(LoginRequiredMixin, UpdateView):
   def get(self, request, **kwargs):
-    queryset = request.GET.get("Buscar")
-    accioni = Accionista.accionistas.all()
-    if queryset:
-      accioni = Accionista.accionistas.filter(
-        Q(nombres__icontains = queryset) |
-        Q( apellidos__icontains = queryset) |
-        Q( run__icontains = queryset)
-      ).distinct()
-
-    return render(request,'busqueda.html',{'accioni': accioni})
+    return render(request,'Busqueda.html',{'accioni': accioni})
