49ca4e4f9120b8266e76306e8bdf19f8d6123c15
jcvron <ronalo.25.98@gmail.com>
2019-09-30 15:58:41 -0300

    Arreglo de la carga de imagenes y el html de francisco

--- seguridad/urls.py
+++ seguridad/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url,re_path
 from django.urls import path, include
 from seguridad import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   url(r'^$',views.HomePageView.as_view(),name="index"),
   path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
