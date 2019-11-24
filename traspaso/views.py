f9d339259e476f57b84fb72f06f981852944a64d
Tostin <49213652+Tostin@users.noreply.github.com>
2019-11-23 22:50:07 -0300

    Ingreso busqueda

--- traspaso/views.py
+++ traspaso/views.py
@@ -3,8 +3,20 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from traspaso.models import Acciones
+from accionista.models import Accionista
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
+
+class TraspasoView(LoginRequiredMixin, TemplateView):
+  def get(self, request, **kwargs):
+      queryset = request.GET.get("Buscar")
+      acciones = Acciones.acciones.all()
+      accionistas = Accionista.accionista.all()
+      if queryset:
+        acciones = Accionista.accionistas.filter(
+          Q( run__icontains = queryset)
+        ).distinct()
+    return render(request, 'traspaso.html', {'acciones' : acciones}, {'accionista' : accionista})
 
 
 class AccionesView(LoginRequiredMixin, TemplateView):
