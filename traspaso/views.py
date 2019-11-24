f5484ca42dfb594f50176133038605379fa93c51
Tostin <49213652+Tostin@users.noreply.github.com>
2019-11-24 16:51:43 -0300

    prueba de acciones

--- traspaso/views.py
+++ traspaso/views.py
@@ -24,7 +24,7 @@ class AccionesView(LoginRequiredMixin, TemplateView):
 
 class AccionesView2(LoginRequiredMixin, TemplateView):
     def get(self, request, **kwargs):
-        accioness = Acciones.acciones.all()
+        accioness = Acciones.acciones.filter()
         return render(request, 'editar3.html', {'acciones' : accioness})
 
 class CreateAcciones(LoginRequiredMixin, CreateView):
