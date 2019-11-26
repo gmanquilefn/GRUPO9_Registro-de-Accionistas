b50f03f7ad5e242e714c344027dcfbe001e17a42
PedroConejera <pedroconejerab@gmail.com>
2019-11-25 18:54:18 -0800

    Fix migrations

--- tercero/models.py
+++ tercero/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from accionista.models import Accionista
 
 ROL_CHOICES = (
     ('apoderado','APODERADO'),
@@ -10,7 +9,7 @@ ROL_CHOICES = (
 
 class Tercero(models.Model):
   tercero_id = models.AutoField(primary_key=True)
-  accionista = models.ForeignKey(Accionista, on_delete=models.CASCADE)
+  accionista_id = models.ManyToManyField('accionista.Accionista', blank=False)
   run = models.CharField(max_length=20, null=False, blank=False)
   nombres = models.CharField(max_length=100, null=False)
   apellidos = models.CharField(max_length=100, null=False)
